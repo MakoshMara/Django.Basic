918262e192dea85f0e851b070a0edc3d464bd4a5
PanovaM <ri5@mail.ru>
2021-03-25 18:46:34 +0700

    DZ8

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 import json
 
@@ -33,7 +34,7 @@ def main(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     links_menu = ProductCategory.objects.all()
     if pk is not None:
         if pk == 0:
@@ -43,10 +44,19 @@ def products(request, pk=None):
             category_item = get_object_or_404(ProductCategory, pk=pk)
             products_list = Product.objects.filter(category=category_item)
 
+        paginator = Paginator(products_list, 2)
+
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
         content = {
             'title': 'Товары',
             'links_menu': links_menu,
-            'products': products_list,
+            'products': products_paginator,
             'category': category_item,
             'basket': get_basket(request.user)
         }
